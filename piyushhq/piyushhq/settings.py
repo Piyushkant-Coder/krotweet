c925599f92c83e2f020d74624ea7055ed4d74dc9
Piyushkant-Coder <coder@example.com>
2025-12-18 21:00:36 +0530

    Fix static files config

--- piyushhq/piyushhq/settings.py
+++ piyushhq/piyushhq/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("d2ef4abf88c23bfa613dcaef686853f1", "unsafe-secret")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
@@ -129,7 +129,8 @@ USE_TZ = True
 # MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
